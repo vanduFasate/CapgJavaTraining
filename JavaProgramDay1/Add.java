ef2a1268d13a04950b7b341538b63af7fa3ef2f9
vanduFasate <105098571+vanduFasate@users.noreply.github.com>
2022-05-07 12:26:10 +0530

    initial commit

---
+++ JavaProgramDay1/Add.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class Add
+{
+public static void main(String arr[])
+{
+int a,b,c;
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter two number");
+a=sc.nextInt();
+b=sc.nextInt();
+c=a+b;
+System.out.println("Addition is: "+c);
+}
+}
---
+++ JavaProgramDay1/Demo.txt
@@ -0,0 +1,6 @@
+class Demo
+{
+public static void main (String arr[])
+{
+System.out.println("hello pratima");
+}}
