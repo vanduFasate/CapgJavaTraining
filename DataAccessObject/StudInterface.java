105271cff3f24304b0b78e9bddb241066852dd1a
vanduFasate <105098571+vanduFasate@users.noreply.github.com>
2022-05-11 16:07:27 +0530

    commit initial

--- DataAccessObject/StudInterface.java
+++
@@ -1,9 +0,0 @@
-package vandudbsql;
-
-public interface StudInterface
-    {
-        void save(Student st);
-        void update(Student st);
-        void delete(Student st);
-        Student getStud(int bnum);
-    }
