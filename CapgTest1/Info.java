1b13a55c76edb516e689d6f618743b81bc13ae08
vanduFasate <105098571+vanduFasate@users.noreply.github.com>
2022-05-12 13:44:47 +0530

    commit initial

---
+++ CapgTest1/Info.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Info
+{
+public static void main(String arg[])
+   {
+      String fname="Vandu",lname="Fasate";
+      String s=fname.toUpperCase()+" "+lname.toUpperCase();
+      System.out.println("your name:"+s);
+      
+   }
+}
