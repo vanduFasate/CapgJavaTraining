06ee9876a61ae31ef76226228796f14aea35bad9
vanduFasate <105098571+vanduFasate@users.noreply.github.com>
2022-05-20 12:59:17 +0530

    commit initial

---
+++ JavaProgramDay38/demomvc/src/main/java/com/example/demomvc/MyController.java
@@ -0,0 +1,17 @@
+package com.example.demomvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class MyController {
+	@GetMapping("/sayhello")
+	public String greeting(@RequestParam(name="name",required=false,defaultValue="DIEMS")String fname,Model model)
+	{
+		model.addAttribute("collegename", fname);
+		return "hello";
+	}
+
+}
